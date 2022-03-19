01f1af42db4b6aa488a03df9bd73dd0b7228b021
Tato <jason.ma1904@gmail.com>
2022-03-19 17:49:50 +1100

    added validation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,7 +7,9 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
